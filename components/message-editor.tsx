7bd4fb6d166d49975b032016275076be7d3f3105
madhavan2025 <madhavan.sykasys@gmail.com>
2026-02-06 15:35:35 +0530

    madd  maddy

--- components/message-editor.tsx
+++ components/message-editor.tsx
@@ -80,9 +80,7 @@ export function MessageEditor({
           onClick={async () => {
             setIsSubmitting(true);
 
-            await deleteTrailingMessages({
-              id: message.id,
-            });
+           
 
             setMessages((messages) => {
               const index = messages.findIndex((m) => m.id === message.id);
