ddd6e61e1d00c1174f5a6bfdb0783dc9a4977b52
madhavan2025 <madhavan.sykasys@gmail.com>
2026-02-11 20:08:04 +0530

    sdy

--- components/FloatingChat.tsx
+++ components/FloatingChat.tsx
@@ -7,6 +7,7 @@ import type { ChatMessage } from "@/lib/types";
 import type { VisibilityType } from "./visibility-selector";
 import { isEmbedMode } from "@/lib/isEmbed";
 import Image from "next/image";
+import { Maximize2, Minimize2 } from "lucide-react";
 interface FloatingChatProps {
   chatId: string;
   initialMessages?: ChatMessage[];
@@ -26,7 +27,7 @@ export const FloatingChat: React.FC<FloatingChatProps> = ({
 }) => {
   const [isOpen, setIsOpen] = useState(false);
    const [isEmbed, setIsEmbed] = useState(false);
-
+     const [isFullScreen, setIsFullScreen] = useState(false);
    useEffect(() => {
     setIsEmbed(isEmbedMode());
   }, []);
@@ -56,11 +57,31 @@ export const FloatingChat: React.FC<FloatingChatProps> = ({
 
       {/* Floating Chat */}
       {isOpen && (
-        <div className="w-[360px] h-[520px] bg-white border rounded-xl shadow-xl flex flex-col overflow-hidden">
-          {/* Header */}
+          <div
+          className={`bg-white border shadow-2xl flex flex-col overflow-hidden transition-all duration-300
+            ${
+              isFullScreen
+                ? "fixed inset-0 rounded-none"
+                : "w-[360px] h-[520px] rounded-xl"
+            }
+          `}
+        >
        <div className="flex items-center justify-between px-4 py-3 bg-[#6FA8E8] text-white">
   <span className="font-semibold">Chatbot</span>
+      <div className="flex items-center gap-3">
+              {/* Expand / Minimize Button */}
+              <button
+                onClick={() => setIsFullScreen(!isFullScreen)}
+                className="hover:opacity-80"
+              >
+                {isFullScreen ? (
+                  <Minimize2 size={18} />
+                ) : (
+                  <Maximize2 size={18} />
+                )}
+              </button>
   <button onClick={() => setIsOpen(false)}>✕</button>
+</div>
 </div>
 
 
